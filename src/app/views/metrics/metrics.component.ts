b738ecd0822d3b286d47f97c6884adb238ee55da
MarianaPerezC <a01731813@tec.mx>
2023-05-15 23:22:25 -0600

    valores de las metricas y variables

--- src/app/views/metrics/metrics.component.ts
+++ src/app/views/metrics/metrics.component.ts
@@ -7,7 +7,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./metrics.component.css']
 })
 export class MetricsComponent {
-  constructor(private router: Router){}  
+  constructor(private router: Router){}
+  heart_rate:number= 12;
+  resp_rate:number= 16;
+  oxy_sat:number= 96;
+  weight:number= 58;
+  bmi:number= 22;
+  temperature:number= 35;
+  blood_press:number= 74.5;
 
   storage(){
     this.router.navigate(['storage']);
