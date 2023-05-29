2e7d52e714df2dbabc1798808b0d4c93710be2a4
Salette Noemi Villalobos <A01246619@tec.mx>
2023-05-28 20:48:07 -0600

    form

--- src/app/views/login/auth/auth.component.ts
+++ src/app/views/login/auth/auth.component.ts
@@ -1,25 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { StorageService } from 'src/app/services_/storage.service';
+import { AuthService } from 'src/app/services_/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
-    loginForm!: FormGroup;
 
-    constructor(private router: Router) {}
+export class AuthComponent implements OnInit {
+    //loginForm!: FormGroup;
 
-    ngOnInit(): void {
+    /*form: any = {
+      correo: null,
+      contrasena: null
+    };*/
+
+    isLoggedIn = false;
+    isLoginFailed = false;
+    errorMessage = '';
+    roles: string[] = [];
+
+    constructor(private authService: AuthService, private storageService:StorageService, private router: Router) {}
+    loginForm!:FormGroup;
+
+
+    
+    ngOnInit() {
       this.loginForm = new FormGroup({
         email: new FormControl(''),
         psw: new FormControl(''),
       });
+      if (this.storageService.isLoggedIn()) {
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        const id_cliente = this.storageService.getUser().id;
+        this.router.navigate(['inicio']);
+      }
     }
 
-    login(){
+    
+    onSubmit(form: FormGroup) {
+      const email = form.value.email;
+      const psw = form.value.psw
+      
+      this.authService.login(email, psw).subscribe({
+        next: data => {
+          this.storageService.saveUser(data);
+  
+          this.isLoginFailed = false;
+          this.isLoggedIn = true;
+          this.roles = this.storageService.getUser().roles;
+          this.reloadPage();
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isLoginFailed = true;
+        }
+      });
+    }
+
+    reloadPage(): void {
+      window.location.reload();
+    }
+
+    /*login(){
       this.router.navigate(['ucontact']);
     }
     cuenta(){
@@ -27,5 +74,5 @@ export class AuthComponent {
     }
     restore(){
       this.router.navigate(['restore-p']);
-    }
+    }*/
 }
